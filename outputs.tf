d7deb0be7c0a850a9cdabbdc690607102980508c
Unnathi B <unnathib@Mac.lan>
2026-04-19 19:26:52 +0530

    Commit from local

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,29 @@
 output "bucket_url" {
   value = google_storage_bucket.my_bucket.self_link
+  description = "GCS bucket link"
+}
+
+output "vpc_url" {
+  value = google_compute_network.my_vpc.self_link
+  description = "Compute network link"
+}
+
+output "private_subnet_url" {
+  value = google_compute_subnetwork.private_subnet.self_link
+  description = "Private subnet link"
+}
+
+output "private_subnet_state" {
+  value = google_compute_subnetwork.private_subnet.state
+  description = "Private subnet state"
+}
+
+output "public_subnet_url" {
+  value = google_compute_subnetwork.public_subnet.self_link
+  description = "Public subnet link"
+}
+
+output "public_subnet_state" {
+  value = google_compute_subnetwork.public_subnet.state
+  description = "Public subnet state"
 }
--- variable.tf
+++ variable.tf
@@ -36,3 +36,69 @@ variable "object_delete_age" {
   type = number
   default = 30
 }
+
+variable "vpc_name" {
+  type = string
+  default = "test-network"
+}
+
+variable "public_subnet_name" {
+  type = string
+  default = "test-public-subnet"
+}
+
+variable "private_subnet_name" {
+  type = string
+  default = "test-private-subnet"
+}
+
+variable "subnet_cidr_overlap" {
+  type = bool
+  default = false
+}
+
+variable "subnet_ipv6_disable_access" {
+  type = string
+  default = "DISABLE_GOOGLE_ACCESS"
+}
+
+variable "subnet_ipv6_enable_access" {
+  type = string
+  default = "ENABLE_OUTBOUND_VM_ACCESS_TO_GOOGLE"
+}
+
+variable "subnet_purpose" {
+  type = string
+  default = "PRIVATE"
+}
+
+variable "public_subnet_pri_cidr" {
+  type = string
+}
+
+variable "public_subnet_sec_cidr" {
+  type = string
+}
+
+variable "private_subnet_pri_cidr" {
+  type = string
+}
+
+variable "private_subnet_sec_cidr" {
+  type = string
+}
+
+variable "ipv4_stack" {
+  type = string
+  default = "IPV4_ONLY"
+}
+
+variable "dual_stack" {
+  type = string
+  default = "IPV4_IPV6"
+}
+
+variable "vpc_routing" {
+  type = string
+  default = "REGIONAL"
+}
